aa45ce31447f839f7d8af454d2895f8667334aa3
AlexIv <alx.ekb@gmail.com>
2019-08-09 19:49:08 +0500

    Linters

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,4 @@
 // frontend/packs/application.js
 import "./application.pcss";
 
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
+document.body.insertAdjacentHTML("afterbegin", "Hello world!");
