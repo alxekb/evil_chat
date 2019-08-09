ee7c8e06773b28184199d0bce880ceec2cbd19a5
AlexIv <alx.ekb@gmail.com>
2019-08-09 20:21:06 +0500

    Page component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,7 +1,2 @@
-// frontend/packs/application.js
-import "./application.pcss";
-
-// frontend/packs/application.js
 import "init";
-
-document.body.insertAdjacentHTML("afterbegin", "Hello world!");
+import "components/page/page";
